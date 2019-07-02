65921791da3bce200febc720d02c1afd1d5dd722
Raeleus <raymond.ray3k@gmail.com>
2019-07-02 09:43:43 -0700

    Added sand image and ScrollTest

---
+++ demo/src/com/ray3k/tenpatch/demo/desktop/ScrollTest.java
@@ -0,0 +1,64 @@
+package com.ray3k.tenpatch.demo.desktop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class ScrollTest extends ApplicationAdapter {
+    private Stage stage;
+    private Skin skin;
+    
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new ScrollTest(), config);
+    }
+    
+    @Override
+    public void create() {
+        skin = new Skin(Gdx.files.internal("tenpatch.json"));
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+    
+        Table root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+    
+        Table table = new Table();
+        ScrollPane scrollPane = new ScrollPane(table, skin);
+        scrollPane.setFadeScrollBars(false);
+        root.add(scrollPane).grow();
+    
+        Image image = new Image(skin, "sand-ten");
+        image.setScaling(Scaling.stretch);
+        table.add(image).size(600, 800);
+    }
+    
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+    
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        stage.act();
+        stage.draw();
+    }
+    
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
+}
